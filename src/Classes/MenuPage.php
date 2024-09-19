176ef6d582041699b21125c3e17de4f171249c6a
Rafael González <reggon81@gmail.com>
2024-09-19 11:57:15 -0400

    update params and fix property accesses to avoid warnings

--- src/Classes/MenuPage.php
+++ src/Classes/MenuPage.php
@@ -7,52 +7,44 @@ use WordpressPluginFramework\Utils;
 
 class MenuPage implements Base
 {
-
-  public string $slug = '';
   public string $name = '';
   public string $domain = '';
-  public string $route = '';
-  public string $url = '';
   public string $filepath = '';
-  public bool $hidden;
-  private string $callable;
-  /** @var array<Stylesheet|Script> */ public array $inject;
+  public string $slug = '';
+  private string $callable = 'default_view_data_callable';
+  public bool $hidden = false;
+  /** @var array<Stylesheet|Script> */ public array $inject = [];
 
   /** @param (array{
    *    name:string,
-   *    route:string,
-   *    url:string,
-   *    slug:string,
    *    domain:string,
-   *    callable:string,
-   *    hidden:boolean,
-   *    inject:array<Stylesheet|Script>,
+   *    filepath:string,
+   *    slug:?string,
+   *    callable:?string,
+   *    hidden:?boolean,
+   *    inject:?array<Stylesheet|Script>,
    * }) $props */
   function __construct($props)
   {
     $this->name = $props['name'];
     $this->domain = $props['domain'];
-    $slug = !isset($props['slug']) || empty($props['slug'])
-      ? $props['domain']
-      : $props['domain'] . '_' . $props['slug'];
-    $this->slug = $slug;
-    $this->route = $props['route'];
-    $this->url = $props['url'];
-    $this->filepath = $props['route'] . '/index.php';
-    $this->hidden = isset($props['hidden']) ?  $props['hidden'] : false;
-    $this->callable = !isset($props['callable'])
-      ? 'default_view_data_callable'
-      : $props['callable'];
-    $this->inject = isset($props['inject']) ? $props['inject'] : [];
+    $this->filepath = $props['filepath'];
+
+    $slug = $this->domain;
+    if (!empty($props['slug'])) $slug .= '-' . $props['slug'];
+    $this->slug = strtolower($slug);
+
+    if (isset($props['callable'])) $this->callable = $props['callable'];
+    if (isset($props['hidden'])) $this->hidden = $props['hidden'];
+    if (isset($props['inject'])) $this->inject = $props['inject'];
   }
 
   function init()
   {
-
-    if ($_GET['page'] !== $this->slug) return;
-
     add_action('admin_menu', [$this, 'init_menu'], 20);
 
+    if (!key_exists('page', $_GET) || $_GET['page'] !== $this->slug) return;
+    
     foreach ($this->inject as $injected) {
       $injected->init();
     }
